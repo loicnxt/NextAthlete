16d13dfaaf43c6b58ceb008d6f6874a0f934e3b4
loicnxt <contact@lc-trainer.com>
2026-01-28 09:55:30 +0100

    Increase height of components in checkcomp.py

--- checkcomp.py
+++ checkcomp.py
@@ -204,6 +204,6 @@ html_code = """
 </html>
 """
 
-components.html(html_code, height=200, scrolling=False)
+components.html(html_code, height=700, scrolling=False)
 
 st.caption("Next Athlete Performance System")
