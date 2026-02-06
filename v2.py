d28141c804cb657f438f75b277906f8fae59582c
loicnxt <contact@lc-trainer.com>
2026-02-06 19:07:55 +0100

    Update v2.py

--- v2.py
+++ v2.py
@@ -485,7 +485,7 @@ html_code = """
                 return () => mediaQuery.removeEventListener('change', checkDarkMode);
             }, []);
 
-            return React.createElement('div', { className: 'min-h-screen transition-colors ' + (isDark ? '#0e1117' : 'bg-white') },
+            return React.createElement('div', { className: 'min-h-screen transition-colors ' + (isDark ? 'bg-black' : 'bg-white') },
                 React.createElement('div', { className: 'max-w-md mx-auto p-4 pt-6' },
                     React.createElement('select', {
                         value: currentTool,
